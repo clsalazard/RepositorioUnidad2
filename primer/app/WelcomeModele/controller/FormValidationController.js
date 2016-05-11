9d8143c8dd55f54d669aff9ca72f8729a483ce0f
cl.salazard <cl.salazard@alumnos.duoc.cl>
2016-05-11 19:49:31 -0300

    11-05-16

--- primer/app/WelcomeModele/controller/FormValidationController.js
+++ primer/app/WelcomeModele/controller/FormValidationController.js
@@ -1,3 +1,25 @@
 /**
  * Created by csalazar on 02/05/2016.
  */
+//Generar nuestro modulo
+
+var app = angular.module('LoginFormModule',[]);
+
+
+//Controller
+app.controller('LoginFormController',function ($scope,$http){
+    //modelo, entidad generada para sustentar la vista
+    $scope.formModel = {};
+
+    //funcion que llama al submit
+
+    $scope.onSubmit = function(){
+      console.log("Llegue, !primer submit con AngularJs");
+                    console.log($scope.formModel);
+
+        //mas adelante, veremos llamadas http
+
+    };
+
+    
+});
